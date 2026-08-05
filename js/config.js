db76296c596f2bb47c85a12cc083c1bc20b85a9f
SAS003 <sarvari@atw.hu>
2026-08-05 16:35:49 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+
+    SUPABASE_URL:
+        "https://syxicldefpjdflrhynbr.supabase.co",
+
+    SUPABASE_ANON_KEY:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5eGljbGRlZnBqZGZscmh5bmJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyMTgzMzAsImV4cCI6MjA5Mzc5NDMzMH0.YetlXpauo_zVHQvCzs_0aRRD10JqIeYRiJxlonApEUE"
+
+};
