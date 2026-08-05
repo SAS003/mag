faab4b56a44b0a24b4d258f8ddf333195160c742
SAS003 <sarvari@atw.hu>
2026-08-05 16:38:25 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,19 @@
+/*
+==========================================================
+MAG v0.2
+config.js
+==========================================================
+*/
+
 const CONFIG = {
 
     SUPABASE_URL:
         "https://syxicldefpjdflrhynbr.supabase.co",
 
     SUPABASE_ANON_KEY:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5eGljbGRlZnBqZGZscmh5bmJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyMTgzMzAsImV4cCI6MjA5Mzc5NDMzMH0.YetlXpauo_zVHQvCzs_0aRRD10JqIeYRiJxlonApEUE"
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5eGljbGRlZnBqZGZscmh5bmJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyMTgzMzAsImV4cCI6MjA5Mzc5NDMzMH0.YetlXpauo_zVHQvCzs_0aRRD10JqIeYRiJxlonApEUE",
+
+     TABLE:
+        "cki_conversations"
 
 };
